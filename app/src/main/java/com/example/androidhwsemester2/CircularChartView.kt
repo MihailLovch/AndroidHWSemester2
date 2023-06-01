1f43712e40c58ee485a47a88824b2d1f402b6acf
Mihail Lovchitskiy <misha_aid@mail.ru>
2023-06-01 14:15:59 +0300

    moved calculations from onDraw

--- app/src/main/java/com/example/androidhwsemester2/CircularChartView.kt
+++ app/src/main/java/com/example/androidhwsemester2/CircularChartView.kt
@@ -18,11 +18,17 @@ class CircularChartView @JvmOverloads constructor(context: Context, attrs: Attri
     private val strokeWidth = 40f
     private val chartRect = RectF()
 
+    private var centerX: Float = 0f
+    private var centerY: Float = 0f
+    private var radius: Float = 0f
+
     var data: List<Pair<Int, Int>> = emptyList()
         set(value) {
             field = value
             invalidate()
         }
+
+    private var segmentsData = mutableListOf<Segment>()
 
     private val chartPaint = Paint().apply {
         style = Paint.Style.FILL
@@ -36,22 +42,14 @@ class CircularChartView @JvmOverloads constructor(context: Context, attrs: Attri
         typeface = Typeface.DEFAULT_BOLD
     }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        val centerX = width.toFloat() / 2
-        val centerY = height.toFloat() / 2
-        val radius = (min(width, height) / 2) - (strokeWidth / 2)
-
-        chartRect.set(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
+    private fun calculateSegments() {
+        segmentsData = mutableListOf()
 
         var currentAngle = startAngle
 
         for ((color, percent) in data) {
-            val sweepAngle = (360f * percent) / 100f
 
-            chartPaint.color = color
-            canvas.drawArc(chartRect, currentAngle, sweepAngle, true, chartPaint)
+            val sweepAngle = (360f * percent) / 100f
 
             val textRadius = radius * 0.5f
             val textAngle = currentAngle + (sweepAngle / 2)
@@ -59,11 +57,55 @@ class CircularChartView @JvmOverloads constructor(context: Context, attrs: Attri
                 centerX + (textRadius * cos(Math.toRadians(textAngle.toDouble()))).toFloat()
             val textY =
                 centerY + (textRadius * sin(Math.toRadians(textAngle.toDouble()))).toFloat()
-
             val text = "$percent%"
-            canvas.drawText(text, textX, textY, textPaint)
+
+            segmentsData.add(
+                Segment(
+                    startAngle = currentAngle,
+                    endAngle = sweepAngle,
+                    color = color,
+                    text = text,
+                    textX = textX,
+                    textY = textY
+                )
+            )
 
             currentAngle += sweepAngle
         }
     }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        centerX = width.toFloat() / 2
+        centerY = height.toFloat() / 2
+        radius = (min(width, height) / 2) - (strokeWidth / 2)
+
+        chartRect.set(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
+
+        setMeasuredDimension(width,height)
+
+        calculateSegments()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        for (segment in segmentsData) {
+            with(segment) {
+                chartPaint.color = color
+                canvas.drawArc(chartRect, startAngle, endAngle, true, chartPaint)
+                canvas.drawText(text, textX, textY, textPaint)
+            }
+        }
+    }
 }
+
+private data class Segment(
+    val startAngle: Float,
+    val endAngle: Float,
+    val color: Int,
+    val text: String,
+    val textX: Float,
+    val textY: Float,
+)
